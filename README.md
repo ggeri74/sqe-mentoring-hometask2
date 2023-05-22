9fe5122d679318753fe2a0961ece0ce160f9c6ab
Gergely Glosz <gergely_glosz@epam.com>
2023-05-22 21:39:54 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # sqe-mentoring-hometask2
 Create a basic API test automation framework using the resources of the https://gorest.co.in/ website!
+
+How to install:
+
+git clone git@github.com:ggeri74/sqe-mentoring-hometask2.git
+
+cd sqe-mentoring-hometask2/
+
+npm install --save-dev  cypress
+
+npx cypress open
---
+++ cypress/e2e/post.cy.js
@@ -0,0 +1,81 @@
+describe("POST request test", () => {
+
+  const authHeader = "Bearer ae45dfa0e871e65f3c9978083274d9c6c406989da91fe63aee244bd99b42a3b0";
+  const baseUrl = "https://gorest.co.in/public/v2/users/";
+  let userId;
+  const name = "Oriza Triznyak";
+  const email = "oriza.triznyak@example.com";
+  const gender = "male";
+  const status = "active";
+
+  it("Creates a new user", () => {
+    cy.request({
+      method: "POST",
+      url: baseUrl,
+      headers: {
+        Authorization: authHeader,
+      },
+      body: {
+        name: name,
+        email: email,
+        gender: gender,
+        status: status,
+      },
+    }).then((response) => {
+      expect(response.status).to.eq(201);
+      expect(response.headers).to.have.property("server", "cloudflare");
+      expect(response.body).to.have.property("name", name);
+      expect(response.body).to.have.property("email", email);
+      expect(response.body).to.have.property("gender", gender);
+      expect(response.body).to.have.property("status", status);
+      expect(response.body).to.have.property("id");
+      userId = response.body.id;
+    });
+  });
+
+  it("Retrieves the user data", () => {
+    cy.request({
+      method: "GET",
+      url: baseUrl + userId,
+      headers: {
+        Authorization: authHeader,
+      },
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body.id).to.eq(userId);
+      expect(response.body.name).to.eq(name);
+      expect(response.body.email).to.eq(email);
+      expect(response.body.gender).to.eq(gender);
+      expect(response.body.status).to.eq(status);
+    });
+  });
+
+  it("Updates the user status to inactive", () => {
+    cy.request({
+      method: "PATCH",
+      url: baseUrl + userId,
+      headers: {
+        Authorization: authHeader,
+      },
+      body: {
+        status: "inactive",
+      },
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body.status).to.eq("inactive");
+    });
+  });
+
+  it("Deletes the user", () => {
+    cy.request({
+      method: "DELETE",
+      url: baseUrl + userId,
+      headers: {
+        Authorization: authHeader,
+      },
+    }).then((response) => {
+      expect(response.status).to.eq(204);
+    });
+  });
+
+});
