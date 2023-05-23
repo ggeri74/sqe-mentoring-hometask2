dc3eaf87ea7a4155b6b1e1113ab40c0b005d783c
Gergely Glosz <gergely_glosz@epam.com>
2023-05-24 01:05:16 +0200

    adding 2nd resource

--- cypress/e2e/api-tests.cy.js
+++ cypress/e2e/api-tests.cy.js
@@ -1,19 +1,19 @@
 describe("API tests", () => {
 
   const authHeader = "Bearer ae45dfa0e871e65f3c9978083274d9c6c406989da91fe63aee244bd99b42a3b0";
-  const baseUrl = "https://gorest.co.in/public/v2/users/";
-  let userId;
-  const name = "Oriza Triznyak";
-  const email = "oriza.triznyak@example.com";
+  const baseUrl = "https://gorest.co.in/public/v2/";
+  const name = "Ugrifules";
+  const email = "ugrifules@example.com";
   const gender = "male";
   const status = "active";
+  let userId;
 
-  it("POST Creates a new user", () => {
+  before(() => {
     cy.request({
       method: "POST",
-      url: baseUrl,
+      url: baseUrl + "users",
       headers: {
-        Authorization: authHeader,
+        Authorization: authHeader
       },
       body: {
         name: name,
@@ -23,58 +23,99 @@ describe("API tests", () => {
       },
     }).then((response) => {
       expect(response.status).to.eq(201);
-      expect(response.headers).to.have.property("server", "cloudflare");
-      expect(response.body).to.have.property("name", name);
-      expect(response.body).to.have.property("email", email);
-      expect(response.body).to.have.property("gender", gender);
-      expect(response.body).to.have.property("status", status);
-      expect(response.body).to.have.property("id");
       userId = response.body.id;
     });
   });
 
-  it("GET Retrieves the user data", () => {
-    cy.request({
-      method: "GET",
-      url: baseUrl + userId,
-      headers: {
-        Authorization: authHeader,
-      },
-    }).then((response) => {
-      expect(response.status).to.eq(200);
-      expect(response.body.id).to.eq(userId);
-      expect(response.body.name).to.eq(name);
-      expect(response.body.email).to.eq(email);
-      expect(response.body.gender).to.eq(gender);
-      expect(response.body.status).to.eq(status);
-    });
-  });
-
-  it("PATCH Updates the user status to inactive", () => {
-    cy.request({
-      method: "PATCH",
-      url: baseUrl + userId,
-      headers: {
-        Authorization: authHeader,
-      },
-      body: {
-        status: "inactive",
-      },
-    }).then((response) => {
-      expect(response.status).to.eq(200);
-      expect(response.body.status).to.eq("inactive");
-    });
-  });
-
-  it("DELETE Deletes the user", () => {
+  after(() => {
     cy.request({
       method: "DELETE",
-      url: baseUrl + userId,
+      url: baseUrl + "users/" + userId,
       headers: {
-        Authorization: authHeader,
+        Authorization: authHeader
       },
     }).then((response) => {
       expect(response.status).to.eq(204);
+    });
+  });
+
+  describe("Testing the /users resource", () => {
+
+    it("GET Retrieves the user created in the Before hook", () => {
+      cy.request({
+        method: "GET",
+        url: baseUrl + "users/" + userId,
+        headers: {
+          Authorization: authHeader
+        },
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.headers.server).to.eq("cloudflare");
+        expect(response.body.id).to.eq(userId);
+        expect(response.body.name).to.eq(name);
+        expect(response.body.email).to.eq(email);
+        expect(response.body.gender).to.eq(gender);
+        expect(response.body.status).to.eq(status);
+      });
+    });
+
+    it("PATCH Updates the user name", () => {
+      let updatedName = "Legkisebb Ugrifules";
+      cy.log("userId: " + userId);
+      cy.request({
+        method: "PATCH",
+        url: baseUrl + "users/" + userId,
+        headers: {
+          Authorization: authHeader
+        },
+        body: {
+          name: updatedName,
+        },
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body.name).to.eq(updatedName);
+      });
+    });
+  });
+
+  describe("Testing the /users/<userId>/posts resource", () => {
+
+    const title = "Test. Automate. Accelerate."
+    const body = "With Cypress, you can easily create tests for your modern web applications, debug them visually, and automatically run them in your continuous integration builds."
+    let postId;
+
+    it("POST Creates a new post by the user", () => {
+      cy.request({
+        method: "POST",
+        url: baseUrl + "users/" + userId + "/posts",
+        body: {
+          user_id: userId,
+          title: title,
+          body: body
+        },
+        headers: {
+          Authorization: authHeader
+        }
+      }).then((response) => {
+        expect(response.status).to.equal(201);
+        postId = response.body.id;
+      });
+    });
+
+    it("GET Retrieves the post created by the user", () => {
+      cy.request({
+        method: "GET",
+        url: baseUrl + "users/" + userId + "/posts",
+        headers: {
+          Authorization: authHeader
+        },
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body[0].id).to.eq(postId);
+        expect(response.body[0].user_id).to.eq(userId);
+        expect(response.body[0].title).to.eq(title);
+        expect(response.body[0].body).to.eq(body);
+      });
     });
   });
 
